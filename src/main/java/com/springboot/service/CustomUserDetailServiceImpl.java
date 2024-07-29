2c035b0e3fb297bed9568afc2b63cf94c6f18b42
rauniyaranish <anishroniyar@gmail.com>
2024-07-29 16:52:20 +0545

    Custom user details service pushed

---
+++ src/main/java/com/springboot/service/CustomUserDetailServiceImpl.java
@@ -0,0 +1,27 @@
+package com.springboot.service;
+
+import com.springboot.entity.User;
+import com.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRoles().toArray(new String[0]))
+                .build();
+    }
+}
